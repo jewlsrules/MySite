a3aad34c0111f209cfa5465ede0fa9dcc39455b2
Jewls Krueger <jewls@Jewlss-MacBook-Pro.local>
2019-11-22 11:28:15 -0800

    added public files and finished setting up server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
+
+// Port ===============
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log('listening on port', PORT)
+});
+
+// Middleware =========
+app.use(express.json());
+app.use(express.static('public'));
